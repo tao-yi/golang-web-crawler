6849da0cb8433673ea4aca332ecd1a2815608657
Tao Yi <tao.yi@treelab.co>
2021-01-17 14:20:19 +0800

    Add html content encoding detect

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module golang-web-crawl
 
 go 1.15
+
+require (
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
+	golang.org/x/text v0.3.5
+)
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
 func main() {
@@ -19,9 +26,23 @@ func main() {
 		fmt.Printf("Error status code:%d", resp.StatusCode)
 	}
 
-	content, err := ioutil.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := detectEncoding(bodyReader)
+
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	content, err := ioutil.ReadAll(utf8Reader)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s", content)
+}
+
+func detectEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		log.Printf("fetch error:%v", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
